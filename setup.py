91dbea7b55551e5778ab1fd1b385c5434286a89b
Gazn000 <106374728+Gazn000@users.noreply.github.com>
2025-06-03 12:04:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_cityflow', # name of the package, used like this: `import gym_cityflow`
       version='0.1',
-      install_requires=['gym', 'cityflow'] # And any other dependencies required
+      install_requires=['gym', 'cityflow'] # any other dependencies required
 )
